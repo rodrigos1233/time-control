f508e03c58f48b0d9b6be89da9e9f2e85ba294c0
Rodrigo <rodrigopsalazar@hotmail.com>
2025-01-13 00:53:21 +0100

    update styling

--- src/app/components/selector/selector.tsx
+++ src/app/components/selector/selector.tsx
@@ -39,13 +39,13 @@ export default function Selector({selectedIndex, setSelectedIndex, options}: sel
 
 
     return (
-        <div className="flex bg-black/[.05] rounded-xl p-1 relative border-4 border-black/[.10] gap-1">
+        <div className="flex bg-black/[.05] rounded-xl p-1 relative border-4 border-black/[.10] gap-1 w-full">
             <div className="z-10 absolute bg-amber-500 rounded-lg transition-all" style={{ left: `${optionStyles.left}px`, width: `${optionStyles.width}px`, height: 'calc(100% - 0.5rem)' }}></div>
             {options.map((option, index) => (
                 <div
                     key={index}
                     ref={el => { optionRefs.current[index] = el; }}
-                    className={`${selectedIndex === index ? "hover:bg-transparent cursor-default" : "hover:bg-amber-200 bg-transparent"} relative z-20 rounded-lg px-4 py-2 text-sm sm:text-base text-center flex-grow cursor-pointer`}
+                    className={`${selectedIndex === index ? "hover:bg-transparent cursor-default" : "hover:bg-amber-200 bg-transparent"} relative z-20 rounded-lg px-6 py-4 text-sm sm:text-base text-center flex-grow cursor-pointer`}
                     onClick={() => handleClick(index)}
                 >
                     {option.label}
