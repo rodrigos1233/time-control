275f37c4114939ced2512b6a0b6eae8f5c76eb02
Rodrigo <rodrigopsalazar@hotmail.com>
2025-01-14 16:29:27 +0100

    import font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from 'next/font/local';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -10,6 +11,11 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const clockFont = localFont({
+    src: './../app/fonts/digital-7.ttf',
+    variable: '--font-clock',
 });
 
 export const metadata: Metadata = {
@@ -25,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${clockFont.variable} antialiased`}
       >
         {children}
       </body>
